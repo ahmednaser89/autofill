0e49f32a7aea62726a587071afa5df8f4b8874cb
ahmednaser89 <ahmednaser89@users.noreply.github.com>
2022-11-29 08:02:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -97,6 +97,7 @@ function addressAutocomplete(containerElement, callback, options) {
         const apiKey = "d5206dc793384328934f011e47ce9aef";
 
         var url = `https://api.geoapify.com/v1/geocode/autocomplete?text=${encodeURIComponent(currentValue)}&format=json&limit=5&apiKey=${apiKey}&filter=circle:-${lon},${lat},50000`;
+alert(url);
 
         fetch(url)
           .then(response => {
